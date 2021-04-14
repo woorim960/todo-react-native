74de6de91317838b3cf362f822c19559d54770d6
박우림 <woorimprog@gmail.com>
2021-04-14 15:17:06 +0900

    앱 초기 설정

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled, { ThemeProvider } from "styled-components/native";
+import { theme } from "./theme";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.background};
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const Title = styled.Text`
+  font-size: 40px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.main};
+  align-self: flex-start;
+  margin: 0px 20px;
+`;
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Title>TODO List</Title>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default App;
